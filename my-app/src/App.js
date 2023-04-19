d863c77315b01b381c7cf86f425819805a882cba
suchakee004 <suchakee001@gmail.com>
2023-04-19 19:05:39 +0700

    run

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,45 @@
 import React from 'react';
 import './App.css';
 
-const handleClick = () => {
-  console.log('me click');
+const handleClick1 = () => {
+  console.log('me click1');
 };
 
+const handleClick2 = () => {
+  console.log('me click2');
+};
+
+const handleClick3 = () => {
+  console.log('me click3');
+};
 function App() {
   return (
 
-    <div class="App">
-      <div class="header"> 
-           <div class="modal">
+    <div className="App">
+      <div className="header"> 
+           <div className="modal">
              <h2>Chrats</h2>
            </div>
-             <button class="button" onClick={handleClick}><h3>Buttom 1</h3></button>
-             <button class="button"><h3>Buttom 2</h3></button>
-             <button class="button"><h3>Buttom 3</h3></button>       
+             <button className="button" onClick={handleClick1}><h3>Buttom 1</h3></button>
+             <button className="button" onClick={handleClick2}><h3>Buttom 2</h3></button>
+             <button className="button" onClick={handleClick3}><h3>Buttom 3</h3></button>       
       </div>
 
-      <div class="body"> 
-        <div class="one">
-          <i class="gg-thermometer"></i>
+      <div className="body"> 
+        <div className="one">
+          <i className="gg-thermometer"></i>
           <h1>H1</h1>
-          <div class="pie animate"> 100%</div>
+          <div className="pie animate"> 100%</div>
         </div>
-        <div class="two">
-          <i class="gg-thermometer"></i>
+        <div className="two">
+          <i className="gg-thermometer"></i>
           <h1>H2</h1>
-          <div class="pie animate"> 100%</div>
+          <div className="pie animate"> 100%</div>
         </div>
-        <div class="three">
-          <i class="gg-thermometer"></i>
+        <div className="three">
+          <i className="gg-thermometer"></i>
           <h1>H3</h1>
-          <div class="pie animate"> 100%</div>
+          <div className="pie animate"> 100%</div>
         </div>
       
       </div>
