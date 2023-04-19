89d348851ae3e9b7b7d720ce5b65fdcb8d5bb3a5
suchakee004 <suchakee001@gmail.com>
2023-04-19 16:40:21 +0700

    run

---
+++ my-app/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+// import { ICONNAME } from 'css.gg'
+
+
+function App() {
+  return (
+
+    <div class="App">
+      <div class="header"> 
+        <div class="container">
+           <div class="modal">
+             <h2>Chrats</h2>
+           </div>
+         </div>
+      
+      </div>
+      <div class="body"> 
+        <div class="one">
+          <i class="gg-thermometer"></i>
+          <h1>H1</h1>
+          <div class="pie animate"> 100%</div>
+        </div>
+        <div class="two">
+          <i class="gg-thermometer"></i>
+          <h1>H2</h1>
+          <div class="pie animate"> 100%</div>
+        </div>
+        <div class="three">
+          <i class="gg-thermometer"></i>
+          <h1>H3</h1>
+          <div class="pie animate"> 100%</div>
+        </div>
+      
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
