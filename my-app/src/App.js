9eaa5045198f6b2acb7fa04f704faa2df415b0f1
suchakee004 <suchakee001@gmail.com>
2023-04-21 11:12:57 +0700

    run

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,32 +16,59 @@ function App() {
   return (
 
     <div className="App">
-      <div className="header"> 
-           <div className="modal">
-             <h2>Chrats</h2>
-           </div>
-             <button className="button" onClick={handleClick1}><h3>Buttom 1</h3></button>
-             <button className="button" onClick={handleClick2}><h3>Buttom 2</h3></button>
-             <button className="button" onClick={handleClick3}><h3>Buttom 3</h3></button>       
+      <div className="header">
+
+        <div className="modal">
+          <h2>Chrats</h2>
+        </div>
+        <button className="button" onClick={handleClick1}><h3>Buttom 1</h3></button>
+        <button className="button" onClick={handleClick2}><h3>Buttom 2</h3></button>
+        <button className="button" onClick={handleClick3}><h3>Buttom 3</h3></button>
       </div>
 
-      <div className="body"> 
-        <div className="one">
-          <i className="gg-thermometer"></i>
-          <h1>H1</h1>
-          <div className="pie animate"> 100%</div>
+      <div className="body">
+      <label>
+        <div class="flip-card">
+        <input type="checkbox"  />
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <div className="one">
+                <i className="gg-thermometer"></i>
+                <h1>H1</h1>
+                <div className="pie animate"> 100%</div>
+              </div>
+              <div className="two">
+                <i className="gg-thermometer"></i>
+                <h1>H2</h1>
+                <div className="pie animate"> 100%</div>
+              </div>
+              <div className="three">
+                <i className="gg-thermometer"></i>
+                <h1>H3</h1>
+                <div className="pie animate"> 100%</div>
+              </div>
+            </div>
+            <div class="flip-card-back">
+            <div className="one">
+                <i className="gg-thermometer"></i>
+                <h1>H4</h1>
+                <div className="pie animate"> 100%</div>
+              </div>
+              <div className="two">
+                <i className="gg-thermometer"></i>
+                <h1>H5</h1>
+                <div className="pie animate"> 100%</div>
+              </div>
+              <div className="three">
+                <i className="gg-thermometer"></i>
+                <h1>H6</h1>
+                <div className="pie animate"> 100%</div>
+              </div>
+            
+            </div>
+          </div>
         </div>
-        <div className="two">
-          <i className="gg-thermometer"></i>
-          <h1>H2</h1>
-          <div className="pie animate"> 100%</div>
-        </div>
-        <div className="three">
-          <i className="gg-thermometer"></i>
-          <h1>H3</h1>
-          <div className="pie animate"> 100%</div>
-        </div>
-      
+      </label>
       </div>
     </div>
 
