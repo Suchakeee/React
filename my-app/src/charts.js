be33d01ba0936bee1e5f67573c59a99ad8d55563
suchakee004 <suchakee001@gmail.com>
2023-04-25 10:16:05 +0700

    update code

---
+++ my-app/src/charts.js
@@ -0,0 +1,42 @@
+export default function Charts() {
+
+    return (
+        <>
+        <div class="flip-card-front">
+        <div className="one">
+            <i className="gg-thermometer"></i>
+            <h1>H1</h1>
+            <div className="pie animate"> 100%</div>
+        </div>
+        <div className="two">
+            <i className="gg-thermometer"></i>
+            <h1>H2</h1>
+            <div className="pie animate"> 100%</div>
+            </div>
+        <div className="three">
+            <i className="gg-thermometer"></i>
+            <h1>H3</h1>
+            <div className="pie animate"> 100%</div>
+            </div>
+        </div>
+        <div class="flip-card-back">
+        <div className="one">
+            <i className="gg-thermometer"></i>
+            <h1>H4</h1>
+            <div className="pie animate"> 100%</div>
+        </div>
+        <div className="two">
+            <i className="gg-thermometer"></i>
+            <h1>H5</h1>
+            <div className="pie animate"> 100%</div>
+        </div>
+        <div className="three">
+            <i className="gg-thermometer"></i>
+            <h1>H6</h1>
+            <div className="pie animate"> 100%</div>
+        </div>
+        </div>
+        </>
+
+    );
+}
