9bbae8b832d07d2e6a5e5674dfb6b64f353c19b5
suchakee004 <suchakee001@gmail.com>
2023-04-25 11:08:17 +0700

    run

--- my-app/src/charts.js
+++ my-app/src/charts.js
@@ -5,34 +5,34 @@ export default function Charts() {
         <div class="flip-card-front">
         <div className="one">
             <i className="gg-thermometer"></i>
-            <h1>H1</h1>
+            <h1 style={{padding:'5%'}}>H1</h1>
             <div className="pie animate"> 100%</div>
         </div>
         <div className="two">
             <i className="gg-thermometer"></i>
-            <h1>H2</h1>
+            <h1 style={{padding:'5%'}}>H2</h1>
             <div className="pie animate"> 100%</div>
             </div>
         <div className="three">
             <i className="gg-thermometer"></i>
-            <h1>H3</h1>
+            <h1 style={{padding:'5%'}}>H3</h1>
             <div className="pie animate"> 100%</div>
             </div>
         </div>
         <div class="flip-card-back">
         <div className="one">
             <i className="gg-thermometer"></i>
-            <h1>H4</h1>
+            <h1 style={{padding:'5%'}}>H4</h1>
             <div className="pie animate"> 100%</div>
         </div>
         <div className="two">
             <i className="gg-thermometer"></i>
-            <h1>H5</h1>
+            <h1 style={{padding:'5%'}}>H5</h1>
             <div className="pie animate"> 100%</div>
         </div>
         <div className="three">
             <i className="gg-thermometer"></i>
-            <h1>H6</h1>
+            <h1 style={{padding:'5%'}}>H6</h1>
             <div className="pie animate"> 100%</div>
         </div>
         </div>
