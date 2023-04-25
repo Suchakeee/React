bcc8a33915ef821f10fca43d1f42f8efd596a5c2
suchakee004 <suchakee001@gmail.com>
2023-04-25 10:20:15 +0700

    update code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,24 +16,18 @@ function App() {
         <div className="modal">
           <h2>Chrats</h2>
         </div>
-<<<<<<< HEAD
-        <Button/>
-=======
-        <button className="button" onClick={handleClick1}><h3>Buttom 12321313213131</h3></button>
-        <button className="button" onClick={handleClick2}><h3>Buttom 2</h3></button>
-        <button className="button" onClick={handleClick3}><h3>Buttom 3</h3></button>
->>>>>>> 10a13fad82baccb62da59aafec30d19d36d6348c
+        <Button />
       </div>
 
       <div className="body">
-      <label>
-        <div class="flip-card">
-        <input type="checkbox"  />
-          <div class="flip-card-inner">
-              <Charts/>
+        <label>
+          <div class="flip-card">
+            <input type="checkbox" />
+            <div class="flip-card-inner">
+              <Charts />
+            </div>
           </div>
-        </div>
-      </label>
+        </label>
       </div>
     </div>
 
