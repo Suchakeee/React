0c950c2f1c7b89bbe1ce3a8c71817a4b903fa7ab
suchakee004 <suchakee001@gmail.com>
2023-05-05 22:52:22 +0700

    upgrade code

--- my-app/src/Component.styled.js
+++ my-app/src/Component.styled.js
@@ -13,4 +13,28 @@ export const Container = Styled.button`
     font-size: 15px;
     position: relative;
     overflow: hidden;
+
+    &:hover {
+        color: #000000;
+        transform: scale(1.1);
+        box-shadow: 4px 5px 17px -4px rgb(0, 255, 21);
+        }
+    
+        &:before {
+        content: "";
+        position: absolute;
+        left: -50px;
+        top: 0;
+        width: 0;
+        height: 100%;
+        background-color: rgb(0, 255, 21);;
+        transform: skewX(45deg);
+        z-index: -1;
+        transition: width 1000ms;
+        }
+    
+        &:hover::before {
+        width: 150%;
+        }
+    
 `;
